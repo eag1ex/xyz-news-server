1cdf58ad24bc61203fabc77af10558223902e815
eag1ex <eaglex@eaglex.net>
2021-06-19 12:10:16 +0000

    minor

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// NOTE KEEP this file secure, do not push to public repo
+
 const port = Number(process.env.PORT || 5000)
 
 /* dynamic variable, on local change to development*/
@@ -8,7 +8,7 @@ const path = require('path')
 module.exports = {
     env, // development,production
     port: port,
-    
+
     secret: '345df45657678dgf', //<< NOTE we are NOT using JWT/session !! This is bogus i would not supply it here otherwise !!!
 
     // to run app on local host in production, you need to rebuild it with localhost api
