598bf6be6e083e2716367a99bb42d6e331251543
eag1ex <eaglex@eaglex.net>
2021-06-19 11:31:53 +0000

    minor

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const port = Number(process.env.PORT || 5000)
 
 /* dynamic variable, on local change to development*/
-const env = 'production'
+const env = 'development'
 const path = require('path')
 
 module.exports = {
@@ -11,10 +11,11 @@ module.exports = {
     secret: '345df45657678dgf',
     // NOTE {MY_APP} is a custom var set on heroku to distinguish between environments
     // to run app on local host in production, you need to rebuild it with localhost api
-    HOST: env==='production' ? 'https://immense-ocean-43660.herokuapp.com' : `http://localhost:${port}`,
+    // @ts-ignore
+    HOST: env === 'production' ? 'https://immense-ocean-43660.herokuapp.com' : `http://localhost:${port}`,
     viewsDir: path.join(__dirname, './views'),
     API: {
-        base: 'https://hacker-news.firebaseio.com/v0/'
+        base: 'https://hacker-news.firebaseio.com/v0/',
     },
-    timeout: 5000
+    timeout: 5000,
 }
